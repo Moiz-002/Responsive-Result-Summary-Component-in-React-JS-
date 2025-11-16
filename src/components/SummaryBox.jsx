c045ed8d589ad87d1e3b4bda32050ccf9f5239c2
Moiz-002 <abdulmoizgd@gmail.com>
2025-11-16 12:40:56 +0500

    Initial Commit

--- src/components/SummaryBox.jsx
+++ src/components/SummaryBox.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import iconReaction from '../assets/images/icon-reaction.svg';
+import iconMemory from '../assets/images/icon-memory.svg';
+import iconVerbal from '../assets/images/icon-verbal.svg';
+import iconVisual from '../assets/images/icon-visual.svg';
+
+const imageMap = {
+  'icon-reaction.svg': iconReaction,
+  'icon-memory.svg': iconMemory,
+  'icon-verbal.svg': iconVerbal,
+  'icon-visual.svg': iconVisual,
+};
 
 const SummaryBox = ({icon, category, score, color}) => {
+  const imageSrc = imageMap[icon];
+
   return (
     <div className={`w-full h-full max-h-full flex justify-between p-4 rounded-2xl ${color.bg}`}>
       <div className='flex gap-2  '>
-        <img src={icon} alt="image" />
+        <img src={imageSrc} alt={category} />
         <h4 className={`${color.text} font-medium` }>{category}</h4>
       </div>
       <div>
